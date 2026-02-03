fa4046a52380dc9ae0bb5904734642aa3de64071
Sawan Tanwar <sawantanwar9301@gmail.com>
2026-02-04 02:50:59 +0530

    env based backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
 });
+
+
+
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,7 +1,18 @@
 import { io } from "socket.io-client";
 
 export const connectSocket = (token) => {
-  return io("http://localhost:5000", {
+  return io(import.meta.env.VITE_BACKEND_URL, {
     auth: { token },
+    transports: ["websocket"],
   });
 };
+
+
+
+// import { io } from "socket.io-client";
+
+// export const connectSocket = (token) => {
+//   return io("http://localhost:5000", {
+//     auth: { token },
+//   });
+// };
