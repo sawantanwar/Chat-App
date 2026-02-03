06436c73d68cd3255a67787c6e9c1c40ce5d5f7e
Sawan Tanwar <sawantanwar9301@gmail.com>
2026-02-04 03:01:52 +0530

    third

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./app.css";
+import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
 import { SocketProvider } from "./context/SocketContext";
 
