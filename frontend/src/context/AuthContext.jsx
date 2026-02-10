e47a2cef42616e3cda7339d0bede5481d563b92e
Sawan Tanwar <sawantanwar9301@gmail.com>
2026-02-10 16:02:33 +0530

    final commit

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 import { api } from "../services/api";
-
+import { useEffect } from "react";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
